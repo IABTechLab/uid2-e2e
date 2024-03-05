f4485dd6b8ef99634bc6b37081c3e329202e9326
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2024-03-05 13:51:37 +0800

    Added Core service in apps

--- src/test/java/app/EuidIntegApps.java
+++ src/test/java/app/EuidIntegApps.java
@@ -1,5 +1,6 @@
 package app;
 
+import app.component.Core;
 import app.component.Operator;
 
 import java.util.Set;
@@ -7,7 +8,8 @@ import java.util.Set;
 public final class EuidIntegApps extends Apps {
     public EuidIntegApps() {
         super(Set.of(
-                new Operator("https://integ.euid.eu", "EUID Integ Public Operator", Operator.Type.PUBLIC)
+                new Operator("https://integ.euid.eu", "EUID Integ - Public Operator", Operator.Type.PUBLIC),
+                new Core("http://core.integ.euid.eu", "EUID Integ - Core")
         ));
     }
 }
--- src/test/java/app/LocalPublicApps.java
+++ src/test/java/app/LocalPublicApps.java
@@ -7,7 +7,8 @@ import java.util.Set;
 public final class LocalPublicApps extends Apps {
     public LocalPublicApps() {
         super(Set.of(
-                new Operator("http://localhost", 8080, "Public Operator", Operator.Type.PUBLIC)
+                new Operator("http://localhost", 8080, "Public Operator", Operator.Type.PUBLIC),
+                new Core("http://localhost", 8088, "Core")
         ));
     }
 }
