a73d39f576c3bf868e3c1aae192c934915d47eed
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2025-03-03 16:43:48 +0800

    Added private operator E2E local testing changes

--- src/test/java/suite/core/CoreTest.java
+++ src/test/java/suite/core/CoreTest.java
@@ -6,11 +6,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.uid2.shared.attest.JwtService;
 import com.uid2.shared.attest.JwtValidationResponse;
 import io.vertx.core.json.JsonObject;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled
 public class CoreTest {
     @ParameterizedTest(name = "/attest - {0}")
     @MethodSource({
